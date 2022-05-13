d3bd31f3cb37f26217235e0c3929b977205ac566
gesiomoura <gesiomoura@meto.do>
2022-05-13 09:14:05 -0300

    fix header mobile

--- corebiz-test/src/components/header.tsx
+++ corebiz-test/src/components/header.tsx
@@ -1,5 +1,6 @@
 import logo from '../assets/icons/logo.svg'
 import myAccount from '../assets/icons/myAccount.svg'
+import menu from '../assets/icons/menu.svg'
 import search from '../assets/icons/search.svg'
 import '../styles/header.css'
 import Minicart from './minicart'
@@ -11,30 +12,59 @@ type HeaderProps = {
 export default function Header(props: HeaderProps) {
 
     return (
-        <header className="header-container">
-            <div className="container content">
-                <div className="logo">
-                    <img src={logo} alt="Logo Corebiz" />
+        <>
+            <header className="header-container desktop">
+                <div className="container content">
+                    <div className="logo">
+                        <img src={logo} alt="Logo Corebiz" />
+                    </div>
+                    <div className="search">
+                        <img className='search-icon' src={search} alt="Busca" />
+                        <input
+                            type="search"
+                            placeholder='O que está procurando?'
+                        />
+                    </div>
+                    <div className="content-actions">
+
+                        <div className="account">
+                            <img src={myAccount} alt="Minha Conta" />
+                            <span>Minha Conta</span>
+                        </div>
+
+                        <Minicart
+                            countProducts={props.productsOnCart}
+                        />
+                    </div>
                 </div>
-                <div className="search">
-                    <img className='search-icon' src={search} alt="Busca" />
-                    <input
-                        type="search"
-                        placeholder='O que está procurando?'
-                    />
-                </div>
-                <div className="content-actions">
+            </header>
+            <header className="header-container mobile">
+                <div className="container content">
 
                     <div className="account">
-                        <img src={myAccount} alt="Minha Conta" />
-                        <span>Minha Conta</span>
+                        <img src={menu} alt="Menu" />
+                    </div>
+                    <div className="logo">
+                        <img src={logo} alt="Logo Corebiz" />
                     </div>
 
-                    <Minicart
-                        countProducts={props.productsOnCart}
-                    />
+                    <div className="content-actions">
+
+
+                        <Minicart
+                            countProducts={props.productsOnCart}
+                        />
+                    </div>
+                    <div className="search">
+                        <img className='search-icon' src={search} alt="Busca" />
+                        <input
+                            type="search"
+                            placeholder='O que está procurando?'
+                        />
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+
+        </>
     )
 }
