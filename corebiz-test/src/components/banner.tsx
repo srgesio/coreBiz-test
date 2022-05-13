83b1215d21e0d05d6357371a31cd27ed685656ff
gesiomoura <gesiomoura@meto.do>
2022-05-13 09:22:28 -0300

    fix banner mobile

--- corebiz-test/src/components/banner.tsx
+++ corebiz-test/src/components/banner.tsx
@@ -1,9 +1,15 @@
 import Banner from '../assets/icons/Banner.jpg'
+import BannerMobile from '../assets/icons/BannerMobile.jpg'
 import '../styles/banner.css'
 export default function Banners() {
     return (
-        <div className="banner-container">
-            <img src={Banner} alt="Banner" />
-        </div>
+        <>
+            <div className="banner-container desktop">
+                <img src={Banner} alt="Banner" />
+            </div>
+            <div className="banner-container mobile">
+                <img src={BannerMobile} alt="Banner" />
+            </div>
+        </>
     )
 }
